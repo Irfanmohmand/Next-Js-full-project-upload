8ada8c1f137d073b45ed543d4d5c367846c24c51
Irfan Mohmand <irfanmohmand987@gmail.com>
2025-08-26 08:33:05 -0700

    day13 adding-employee-to-momgodb, get employee and more

--- src/app/api/db-emp/route.jsx
+++ src/app/api/db-emp/route.jsx
@@ -3,12 +3,14 @@ import { Employee } from "@/lib/database/model/employee";
 import mongoose from "mongoose"
 import { NextResponse } from "next/server"
 
+
+// to get the full data of mongodb
 export const GET = async() =>{
     await mongoose.connect(connectionString);
 
     const empData = await Employee.find();
 
-    return NextResponse.json({result: empData})
+    return NextResponse.json({result: empData, success: true})
 }
 
 
@@ -26,5 +28,5 @@ export const POST = async (req) =>{
 
 
     const response = await employeeDetail.save();
-    return NextResponse.json({result: response});
+    return NextResponse.json({result: response, success: true});
 }
---
+++ src/app/mongo-get-emp/page.jsx
@@ -0,0 +1,50 @@
+const getData = async () =>{
+
+    const response = await fetch("http://localhost:3000/api/db-emp");
+    const data = await response.json();
+    // console.log(data.result);
+    if(data.success){
+        return data.result
+        // console.log(data.result);
+        
+    }
+    else{
+        false
+    }
+}
+
+const GetEmployee = async () => {
+
+    const emp = await getData();
+
+  return (
+    <div>
+        <h1>Get Employee in next js 14</h1>
+        <table border={'2'}>
+            <thead>
+                <tr>
+                    <td>Name</td>
+                    <td>Salary</td>
+                    <td>Department</td>
+                </tr>
+            </thead>
+            <tbody>
+            {
+                emp.map((item,i) => (
+                    <tr key={i}>
+                        <td> {item.name} </td>
+                        <td> {item.salary} </td>
+                        <td> {item.department} </td>
+                        
+                    </tr>
+                ))
+            }
+        </tbody>
+        </table>
+        
+       
+    </div>
+  )
+}
+
+export default GetEmployee
